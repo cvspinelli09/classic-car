c83df327dd2d75d54d92169a283a8196b28a9741
cvspinelli <cvspinelli09@gmail.com>
2020-03-28 20:57:39 +0000

    Updates

---
+++ precache-manifest.bf5e491e9ff91a15d5811b2dfdf778e7.js
@@ -0,0 +1,422 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc6d14f32d444c03af9c1afb8b0423a6",
+    "url": "cvspinelli09.github.io/classic-car/index.html"
+  },
+  {
+    "revision": "ac6aec407b2aaeb73009",
+    "url": "cvspinelli09.github.io/classic-car/static/css/main.aeec4324.chunk.css"
+  },
+  {
+    "revision": "b6a6678e5847ee390a67",
+    "url": "cvspinelli09.github.io/classic-car/static/js/2.c26d8eda.chunk.js"
+  },
+  {
+    "revision": "170392c3acad6bbffec743442b1d0ac3",
+    "url": "cvspinelli09.github.io/classic-car/static/js/2.c26d8eda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac6aec407b2aaeb73009",
+    "url": "cvspinelli09.github.io/classic-car/static/js/main.b33aae95.chunk.js"
+  },
+  {
+    "revision": "56d8ebbc8f5c5d296479",
+    "url": "cvspinelli09.github.io/classic-car/static/js/runtime-main.d5c63b64.js"
+  },
+  {
+    "revision": "f9d26c17d96da64eb3ce841b713014b4",
+    "url": "cvspinelli09.github.io/classic-car/static/media/AC_logo.f9d26c17.png"
+  },
+  {
+    "revision": "39b1e7c49550932c638bc7952caa45fb",
+    "url": "cvspinelli09.github.io/classic-car/static/media/ASidderley.39b1e7c4.png"
+  },
+  {
+    "revision": "c8ce13e066f627596bca08bc028c0eda",
+    "url": "cvspinelli09.github.io/classic-car/static/media/Abarth-logo.c8ce13e0.png"
+  },
+  {
+    "revision": "f9cbf7a5aabec9a6417770d1feb82ab2",
+    "url": "cvspinelli09.github.io/classic-car/static/media/Alfa_Romeo_logo.f9cbf7a5.png"
+  },
+  {
+    "revision": "1619b7135ee329399ea96b688eeb368b",
+    "url": "cvspinelli09.github.io/classic-car/static/media/Alpine.1619b713.png"
+  },
+  {
+    "revision": "8944f6d7c810e5b91274fec9024a43f9",
+    "url": "cvspinelli09.github.io/classic-car/static/media/Alvis_logo.8944f6d7.png"
+  },
+  {
+    "revision": "147d26a2366b2a1d818bc83f5ca98626",
+    "url": "cvspinelli09.github.io/classic-car/static/media/Austin.147d26a2.png"
+  },
+  {
+    "revision": "33f9a0af6bf7c19bb2d6f27910ce1203",
+    "url": "cvspinelli09.github.io/classic-car/static/media/Bentley_logo.33f9a0af.png"
+  },
+  {
+    "revision": "18be56de2077154790c4185c60a1a287",
+    "url": "cvspinelli09.github.io/classic-car/static/media/Berkeley.18be56de.png"
+  },
+  {
+    "revision": "0bfe6d7b480c6259df3786611f46b331",
+    "url": "cvspinelli09.github.io/classic-car/static/media/Borgward-logo.0bfe6d7b.png"
+  },
+  {
+    "revision": "69050dc74afe07d4d397e6d5ade06f64",
+    "url": "cvspinelli09.github.io/classic-car/static/media/Gilbern.69050dc7.png"
+  },
+  {
+    "revision": "ec55480ad6edbd0e3569b235980cada7",
+    "url": "cvspinelli09.github.io/classic-car/static/media/Ginetta.ec55480a.png"
+  },
+  {
+    "revision": "033490748c59d6cd76979307abcd31da",
+    "url": "cvspinelli09.github.io/classic-car/static/media/Innocenti.03349074.png"
+  },
+  {
+    "revision": "e3e7806e0e7413af6f5339eccbdb91ee",
+    "url": "cvspinelli09.github.io/classic-car/static/media/Landrover.e3e7806e.png"
+  },
+  {
+    "revision": "73c1628a92e0a391008477c45cf079ef",
+    "url": "cvspinelli09.github.io/classic-car/static/media/Marcos.73c1628a.png"
+  },
+  {
+    "revision": "5ec2d3c65764d2d9ffb17645a5e04481",
+    "url": "cvspinelli09.github.io/classic-car/static/media/Price-guide.5ec2d3c6.png"
+  },
+  {
+    "revision": "1bc38b5daf778484060ffe11178f80c2",
+    "url": "cvspinelli09.github.io/classic-car/static/media/Sunbeam.1bc38b5d.png"
+  },
+  {
+    "revision": "139ce263fff3a0f8df74036bd8478e14",
+    "url": "cvspinelli09.github.io/classic-car/static/media/allard.139ce263.png"
+  },
+  {
+    "revision": "614abbb879a5fd69523a23b72911724f",
+    "url": "cvspinelli09.github.io/classic-car/static/media/aston.614abbb8.png"
+  },
+  {
+    "revision": "18bfdfbc24c5dedc5538678b36ad9293",
+    "url": "cvspinelli09.github.io/classic-car/static/media/audi.18bfdfbc.png"
+  },
+  {
+    "revision": "556761bb9df8fbf0425fa3e984a8f55e",
+    "url": "cvspinelli09.github.io/classic-car/static/media/austin-healey.556761bb.png"
+  },
+  {
+    "revision": "110513de0fd706916a9e1f5885ac7f1f",
+    "url": "cvspinelli09.github.io/classic-car/static/media/bitter.110513de.png"
+  },
+  {
+    "revision": "d58583223dcf65b09a696d5cdffaf8ff",
+    "url": "cvspinelli09.github.io/classic-car/static/media/bizzarrini.d5858322.png"
+  },
+  {
+    "revision": "77e42dfa7bc110f9ca484d2c80f23c96",
+    "url": "cvspinelli09.github.io/classic-car/static/media/bmw.77e42dfa.png"
+  },
+  {
+    "revision": "c7e570881e462a86d961c066ef2442b8",
+    "url": "cvspinelli09.github.io/classic-car/static/media/bristol.c7e57088.png"
+  },
+  {
+    "revision": "5ba13c6647f35dcd6061c7559b013967",
+    "url": "cvspinelli09.github.io/classic-car/static/media/caterham.5ba13c66.png"
+  },
+  {
+    "revision": "955df405e35d5503b387a61e65f4c6ba",
+    "url": "cvspinelli09.github.io/classic-car/static/media/chevrolet.955df405.png"
+  },
+  {
+    "revision": "1787bfe93dbc3b78fad9ba1f0693fbaf",
+    "url": "cvspinelli09.github.io/classic-car/static/media/citroen.1787bfe9.png"
+  },
+  {
+    "revision": "08baf1a3320feb9b30106c2c37c5072d",
+    "url": "cvspinelli09.github.io/classic-car/static/media/daf.08baf1a3.png"
+  },
+  {
+    "revision": "5101a48423bdeb416c5355e85c24c479",
+    "url": "cvspinelli09.github.io/classic-car/static/media/daimler.5101a484.png"
+  },
+  {
+    "revision": "48287ab5521219f44dd5cac38a48c84a",
+    "url": "cvspinelli09.github.io/classic-car/static/media/datsun.48287ab5.png"
+  },
+  {
+    "revision": "055c0c7486a065985bc85c87265ea5b5",
+    "url": "cvspinelli09.github.io/classic-car/static/media/facelvega.055c0c74.png"
+  },
+  {
+    "revision": "b3514c2a76da3e1248e96edfbd95ca45",
+    "url": "cvspinelli09.github.io/classic-car/static/media/ferrari.b3514c2a.png"
+  },
+  {
+    "revision": "5003d47231c2958085ec90965c369114",
+    "url": "cvspinelli09.github.io/classic-car/static/media/fiat.5003d472.png"
+  },
+  {
+    "revision": "a7c627f1f6717bd17e18e7321634520b",
+    "url": "cvspinelli09.github.io/classic-car/static/media/ford.a7c627f1.png"
+  },
+  {
+    "revision": "f74d4a4719a9996be76ae012862051f3",
+    "url": "cvspinelli09.github.io/classic-car/static/media/frazernash.f74d4a47.png"
+  },
+  {
+    "revision": "91b52f68b2f0a40d091bdb756dda5d92",
+    "url": "cvspinelli09.github.io/classic-car/static/media/gordon-keeble.91b52f68.png"
+  },
+  {
+    "revision": "b294e60888bd0b0365e6d2374687b282",
+    "url": "cvspinelli09.github.io/classic-car/static/media/healey.b294e608.png"
+  },
+  {
+    "revision": "673fe435a2d7d6c76b2f340b81acbf01",
+    "url": "cvspinelli09.github.io/classic-car/static/media/hillman.673fe435.png"
+  },
+  {
+    "revision": "0295c3a9f68bba44de7ca0e712606542",
+    "url": "cvspinelli09.github.io/classic-car/static/media/honda.0295c3a9.png"
+  },
+  {
+    "revision": "a9909a6738d0eb5f709eef351c4c5bc7",
+    "url": "cvspinelli09.github.io/classic-car/static/media/humber.a9909a67.png"
+  },
+  {
+    "revision": "e0ea54949688711e0d8f3056c7092924",
+    "url": "cvspinelli09.github.io/classic-car/static/media/iso.e0ea5494.png"
+  },
+  {
+    "revision": "ff0fbf6e73ef8bdb7dd7f72537ed54d2",
+    "url": "cvspinelli09.github.io/classic-car/static/media/isuzu.ff0fbf6e.png"
+  },
+  {
+    "revision": "20bc2510af5139be92a4e23603172b58",
+    "url": "cvspinelli09.github.io/classic-car/static/media/jaguar.20bc2510.png"
+  },
+  {
+    "revision": "b48eca8d081fce8250f82e768d4c9e65",
+    "url": "cvspinelli09.github.io/classic-car/static/media/jensen.b48eca8d.png"
+  },
+  {
+    "revision": "dedad4351cec87b206978cc9b0cbdc86",
+    "url": "cvspinelli09.github.io/classic-car/static/media/jowett.dedad435.png"
+  },
+  {
+    "revision": "b189044e227b8c889f0b042954918b86",
+    "url": "cvspinelli09.github.io/classic-car/static/media/lagonda.b189044e.png"
+  },
+  {
+    "revision": "f385104f8cfe72aa040604c33d18d4c7",
+    "url": "cvspinelli09.github.io/classic-car/static/media/lambo.f385104f.png"
+  },
+  {
+    "revision": "3f58d03b2f257ae28d8a7d481971141c",
+    "url": "cvspinelli09.github.io/classic-car/static/media/lancia.3f58d03b.png"
+  },
+  {
+    "revision": "88bd02fd887e69a6e6af0f2be769ee70",
+    "url": "cvspinelli09.github.io/classic-car/static/media/lea-francis.88bd02fd.png"
+  },
+  {
+    "revision": "ce7ab98ccc4da62ff60b526d2d5f910f",
+    "url": "cvspinelli09.github.io/classic-car/static/media/lotus.ce7ab98c.png"
+  },
+  {
+    "revision": "878e9ea7e5f44519a59450b3b1f10d63",
+    "url": "cvspinelli09.github.io/classic-car/static/media/maserati.878e9ea7.png"
+  },
+  {
+    "revision": "ae24f5f91eff9b36ee4a7793d8d9c094",
+    "url": "cvspinelli09.github.io/classic-car/static/media/matra.ae24f5f9.png"
+  },
+  {
+    "revision": "2e4919ed24268e935bc15c495f9d9c31",
+    "url": "cvspinelli09.github.io/classic-car/static/media/mazda.2e4919ed.png"
+  },
+  {
+    "revision": "4e33305e121dabc21592d523f66c15de",
+    "url": "cvspinelli09.github.io/classic-car/static/media/mclaren.4e33305e.png"
+  },
+  {
+    "revision": "f71c093b2fb23b9dab155d65e76491ef",
+    "url": "cvspinelli09.github.io/classic-car/static/media/mercedes.f71c093b.png"
+  },
+  {
+    "revision": "189eb582ac7be6c21391e0ce81eb48c1",
+    "url": "cvspinelli09.github.io/classic-car/static/media/mg.189eb582.png"
+  },
+  {
+    "revision": "582a5e23e236121bcfa6e57e56872038",
+    "url": "cvspinelli09.github.io/classic-car/static/media/mini.582a5e23.png"
+  },
+  {
+    "revision": "af9c57591b325fadc8559f27c9df4a14",
+    "url": "cvspinelli09.github.io/classic-car/static/media/mitsubishi.af9c5759.png"
+  },
+  {
+    "revision": "6664231792042fa2ad0f9e74ac1fabaf",
+    "url": "cvspinelli09.github.io/classic-car/static/media/monteverdi.66642317.png"
+  },
+  {
+    "revision": "797aa0e6d26e4ba6056e380922f9b878",
+    "url": "cvspinelli09.github.io/classic-car/static/media/morgan.797aa0e6.png"
+  },
+  {
+    "revision": "839f61b7419ea42e77733ae721256c4d",
+    "url": "cvspinelli09.github.io/classic-car/static/media/morris.839f61b7.png"
+  },
+  {
+    "revision": "f804f988f6e101819ec38b2fcc94f3db",
+    "url": "cvspinelli09.github.io/classic-car/static/media/nsu.f804f988.png"
+  },
+  {
+    "revision": "215756b569ad1ac90f369845e3b5fe16",
+    "url": "cvspinelli09.github.io/classic-car/static/media/octane.215756b5.svg"
+  },
+  {
+    "revision": "e8ec0876c4e2825e6cda6fbb65b90c5d",
+    "url": "cvspinelli09.github.io/classic-car/static/media/opel.e8ec0876.png"
+  },
+  {
+    "revision": "63497254e953670f89984343310eb57f",
+    "url": "cvspinelli09.github.io/classic-car/static/media/pagani.63497254.png"
+  },
+  {
+    "revision": "53c97dc1a2748eb127e49e798ae4fcc7",
+    "url": "cvspinelli09.github.io/classic-car/static/media/panhard.53c97dc1.png"
+  },
+  {
+    "revision": "9408d7ad660a1f10f3b859f1be391498",
+    "url": "cvspinelli09.github.io/classic-car/static/media/panther.9408d7ad.png"
+  },
+  {
+    "revision": "9c06504841a1c112046d230d552c1618",
+    "url": "cvspinelli09.github.io/classic-car/static/media/peugeot.9c065048.png"
+  },
+  {
+    "revision": "71468e0e94641b338fa56333685d6923",
+    "url": "cvspinelli09.github.io/classic-car/static/media/plymouth.71468e0e.png"
+  },
+  {
+    "revision": "f9a4387670cf19ed09be52debcb255ca",
+    "url": "cvspinelli09.github.io/classic-car/static/media/pontiac.f9a43876.png"
+  },
+  {
+    "revision": "f495146343120a8da4de56428f81b02a",
+    "url": "cvspinelli09.github.io/classic-car/static/media/porsche.f4951463.png"
+  },
+  {
+    "revision": "c6e5a768ef5f2c2bad28a2a2802b159e",
+    "url": "cvspinelli09.github.io/classic-car/static/media/reliant.c6e5a768.png"
+  },
+  {
+    "revision": "5b9335ad4128dddcf44ffab547a318ee",
+    "url": "cvspinelli09.github.io/classic-car/static/media/renault.5b9335ad.png"
+  },
+  {
+    "revision": "ce09038f5479572566f2b7db2c169ec1",
+    "url": "cvspinelli09.github.io/classic-car/static/media/riley.ce09038f.png"
+  },
+  {
+    "revision": "a1d955b42d58cd8da6d5b2d8d469b6e8",
+    "url": "cvspinelli09.github.io/classic-car/static/media/rochdale.a1d955b4.png"
+  },
+  {
+    "revision": "1d8d756821ea9316362a6d4cba6e2bea",
+    "url": "cvspinelli09.github.io/classic-car/static/media/rollsroyce.1d8d7568.png"
+  },
+  {
+    "revision": "ece1a10c9490346b2dbd092abca2d725",
+    "url": "cvspinelli09.github.io/classic-car/static/media/rover.ece1a10c.png"
+  },
+  {
+    "revision": "f75ae924aa58dbaccb88b757ca771e0b",
+    "url": "cvspinelli09.github.io/classic-car/static/media/saab.f75ae924.png"
+  },
+  {
+    "revision": "c131ab047f88d37dce6c5d1e6c5ba27d",
+    "url": "cvspinelli09.github.io/classic-car/static/media/seat.c131ab04.png"
+  },
+  {
+    "revision": "e3faf21971062cb11c227a9fa92081aa",
+    "url": "cvspinelli09.github.io/classic-car/static/media/shelby.e3faf219.png"
+  },
+  {
+    "revision": "820048b159c22ff54a8a7b9241201672",
+    "url": "cvspinelli09.github.io/classic-car/static/media/simca.820048b1.png"
+  },
+  {
+    "revision": "c62342d56ddde7aebba27a8fd4b93aff",
+    "url": "cvspinelli09.github.io/classic-car/static/media/singer.c62342d5.png"
+  },
+  {
+    "revision": "c4959a1e72c8be8b95aa7cb07ffb9588",
+    "url": "cvspinelli09.github.io/classic-car/static/media/skoda.c4959a1e.png"
+  },
+  {
+    "revision": "bc6af3d49cae7f154fc097306b93bea1",
+    "url": "cvspinelli09.github.io/classic-car/static/media/standard.bc6af3d4.png"
+  },
+  {
+    "revision": "c508b8129c923663a6f384eeb617b0a4",
+    "url": "cvspinelli09.github.io/classic-car/static/media/subaru.c508b812.png"
+  },
+  {
+    "revision": "873aaa9ac91b863235470edfc3f99cda",
+    "url": "cvspinelli09.github.io/classic-car/static/media/suzuki.873aaa9a.png"
+  },
+  {
+    "revision": "cb5f6a88338a202c02d4bda7c54213f0",
+    "url": "cvspinelli09.github.io/classic-car/static/media/swallow.cb5f6a88.png"
+  },
+  {
+    "revision": "47213d0431e19e97d034456c3ed4ffab",
+    "url": "cvspinelli09.github.io/classic-car/static/media/talbot.47213d04.png"
+  },
+  {
+    "revision": "a60bdb893be65cdb5caf582cbed5fdef",
+    "url": "cvspinelli09.github.io/classic-car/static/media/toyota.a60bdb89.png"
+  },
+  {
+    "revision": "e2f14e2bc92250b03c7bc1d5be0039a2",
+    "url": "cvspinelli09.github.io/classic-car/static/media/trident.e2f14e2b.png"
+  },
+  {
+    "revision": "92b44183cf012613dd58dd58b32406ec",
+    "url": "cvspinelli09.github.io/classic-car/static/media/triumph.92b44183.png"
+  },
+  {
+    "revision": "c702c8d4e7882e72f9809cd5fea6ad76",
+    "url": "cvspinelli09.github.io/classic-car/static/media/turner.c702c8d4.png"
+  },
+  {
+    "revision": "4f8c2431628d931e6ca6273c6b6800a3",
+    "url": "cvspinelli09.github.io/classic-car/static/media/tvr.4f8c2431.png"
+  },
+  {
+    "revision": "06b2f77925fec168b351f8236d8bfe3b",
+    "url": "cvspinelli09.github.io/classic-car/static/media/vandenplas.06b2f779.png"
+  },
+  {
+    "revision": "debc32ff823fe441832c6be7cdef880c",
+    "url": "cvspinelli09.github.io/classic-car/static/media/vauxhall.debc32ff.png"
+  },
+  {
+    "revision": "b2692438e60383cfbcb3fcfa81954c82",
+    "url": "cvspinelli09.github.io/classic-car/static/media/volvo.b2692438.png"
+  },
+  {
+    "revision": "bf7324ab2e54e34b92243d909629a27f",
+    "url": "cvspinelli09.github.io/classic-car/static/media/vw.bf7324ab.png"
+  },
+  {
+    "revision": "97ee4167c45d2e6cfdc9e8f40b7f9174",
+    "url": "cvspinelli09.github.io/classic-car/static/media/wolseley.97ee4167.png"
+  }
+]);
